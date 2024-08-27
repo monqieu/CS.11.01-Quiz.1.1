76f8351c92d9882ba9751e85b75f213a4f3cf653
monqieu <moniquesiu319@gmail.com>
2024-08-27 14:27:11 +0800

    finished first lab

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,31 @@ import java.util.Scanner;
 *****************************************************************************************************
 * By writing your name in the space provided below, you are honouring the CIS academic honesty pledge 
 * “I have neither given nor received unauthorized aid on this piece of work.”
-* NAME: 
+* NAME: Monique Siu
 *****************************************************************************************************
 */
 
 public class Main {
 
     public static void main(String[] args) {
-
-
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your age:");
+        int age = -1;
+        while (age < 0) {
+            try {
+                age = Integer.parseInt(scanner.nextLine());
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Please enter a valid number!");
+            }
+        }
+        System.out.println("Please enter your first name:");
+        String firstName = scanner.nextLine();
+        System.out.println("Please enter your favourite food:");
+        String favouriteFood = scanner.nextLine();
+        System.out.println("Name: " + firstName);
+        System.out.println("Age: " + age);
+        System.out.println("Favourite food: " + favouriteFood);
 
     }
-
 }
